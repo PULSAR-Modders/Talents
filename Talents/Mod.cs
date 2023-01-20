485697b438671ae254751a2a21d8554a5a39e731
Mest <mestevenemail@gmail.com>
2023-01-20 13:57:14 +0000

    Add project files.

---
+++ Talents/Mod.cs
@@ -0,0 +1,139 @@
+﻿using HarmonyLib;
+using PulsarModLoader;
+using PulsarModLoader.Content.Components.Reactor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using static PulsarModLoader.Patches.HarmonyHelpers;
+
+namespace Talents
+{
+    public class Mod : PulsarMod
+    {
+        public override string HarmonyIdentifier() => "Mest.Talents";
+        public override string Author => "Mest";
+        public override string Name => "TalentsPlus";
+        public override string Version => "0.0.0";
+
+        public static Dictionary<int, List<ETalents>> cachedTalentsForClass = new Dictionary<int, List<ETalents>>();
+        public static List<ETalents> TalentsForClass(int inClassID)
+        {
+            if (cachedTalentsForClass.ContainsKey(inClassID))
+            {
+                return cachedTalentsForClass[inClassID];
+            }
+            List<ETalents> list = new List<ETalents>();
+            list.Add(ETalents.PISTOL_DMG_BOOST);
+            list.Add(ETalents.QUICK_RESPAWN);
+            list.Add(ETalents.ADVANCED_OPERATOR);
+            list.Add(ETalents.SENSOR_DISH_CERT);
+            list.Add(ETalents.WPNS_RELOAD_SPEED);
+            list.Add(ETalents.WPNS_RELOAD_SPEED_2);
+            list.Add(ETalents.INC_STAMINA);
+            list.Add(ETalents.INC_MAX_WEIGHT);
+            list.Add(ETalents.INC_JETPACK);
+            list.Add(ETalents.INC_TURRET_ZOOM);
+            list.Add(ETalents.INC_HEALING_RATE);
+            list.Add(ETalents.INC_ENEMY_ATRIUM_HEAL);
+            list.Add(ETalents.INC_ALLOW_ENCUMBERED_SPRINT);
+            list.Add(ETalents.ANTI_RAD_INJECTION);
+            list.Add(ETalents.SCI_SCANNER_PICKUPS);
+            list.Add(ETalents.SCI_SCANNER_RESEARCH_MAT);
+            list.Add(ETalents.ITEM_UPGRADER_OPERATOR);
+            list.Add(ETalents.COMPONENT_UPGRADER_OPERATOR);
+            switch (inClassID)
+            {
+                case 0:
+                    list.Add(ETalents.CAP_CREW_SPEED_BOOST);
+                    list.Add(ETalents.CAP_SCAVENGER);
+                    list.Add(ETalents.CAP_ARMOR_BOOST);
+                    list.Add(ETalents.CAP_DIPLOMACY);
+                    list.Add(ETalents.CAP_INTIMIDATION);
+                    list.Add(ETalents.CAP_SCREEN_DEFENSE);
+                    list.Add(ETalents.CAP_SCREEN_SAFETY);
+                    break;
+                case 1:
+                    list.Add(ETalents.PIL_SHIP_TURNING);
+                    list.Add(ETalents.PIL_SHIP_SPEED);
+                    list.Add(ETalents.PIL_REDUCE_SYS_DAMAGE);
+                    list.Add(ETalents.PIL_REDUCE_HULL_DAMAGE);
+                    list.Add(ETalents.PIL_KEEN_EYES);
+                    break;
+                case 2:
+                    list.Add(ETalents.SCI_HEAL_NEARBY);
+                    list.Add(ETalents.SCI_SENSOR_BOOST);
+                    list.Add(ETalents.SCI_SENSOR_HIDE);
+                    list.Add(ETalents.SCI_FREQ_AMPLIFIER);
+                    list.Add(ETalents.SCI_RESEARCH_SPECIALTY);
+                    list.Add(ETalents.SCI_PROBE_COOLDOWN);
+                    list.Add(ETalents.SCI_PROBE_XP);
+                    break;
+                case 3:
+                    list.Add(ETalents.WPNS_TURRET_BOOST);
+                    list.Add(ETalents.WPNS_MISSILE_EXPERT);
+                    list.Add(ETalents.WPNS_COOLING);
+                    list.Add(ETalents.WPNS_REDUCE_PAWN_DMG);
+                    list.Add(ETalents.WPNS_BOOST_CREW_TURRET_CHARGE);
+                    list.Add(ETalents.WPNS_BOOST_CREW_TURRET_DAMAGE);
+                    list.Add(ETalents.WPN_SCREEN_HACKER);
+                    list.Add(ETalents.WPN_AMMO_BOOST);
+                    list.Add(ETalents.E_TURRET_COOLING_CREW_WEAPONS);
+                    break;
+                case 4:
+                    list.Add(ETalents.ENG_COOLANT_MIX_CUSTOM);
+                    list.Add(ETalents.ENG_FIRE_REDUCTION);
+                    list.Add(ETalents.ENG_REPAIR_DRONES);
+                    list.Add(ETalents.ENG_WARP_CHARGE_BOOST);
+                    list.Add(ETalents.ENG_AUX_POWER_BOOST);
+                    list.Add(ETalents.ENG_SALVAGE);
+                    list.Add(ETalents.ENG_COREPOWERBOOST);
+                    list.Add(ETalents.ENG_CORECOOLINGBOOST);
+                    list.Add(ETalents.E_TURRET_COOLING_CREW_ENGINEER);
+                    break;
+            }
+            cachedTalentsForClass[inClassID] = list;
+            return list;
+        }
+
+        public static Dictionary<int, List<ETalents>> cachedTalentsForSpecies = new Dictionary<int, List<ETalents>>();
+        public static List<ETalents> TalentsForSpecies(PLPlayer Player)
+        {
+            bool Gender_IsMale = Player.Gender_IsMale; // True if not Human
+            int RaceID = Player.RaceID; // 0 = Human, 1 = Sylvassi, 2 = Robot
+            if (RaceID == 0 && Gender_IsMale) RaceID = 3; // Makes Female as RaceID 3 (Local)
+            if (cachedTalentsForSpecies.ContainsKey(RaceID))
+            {
+                return cachedTalentsForSpecies[RaceID];
+            }
+            List<ETalents> list = new List<ETalents>();
+            // Default Species Talents
+            list.Add(ETalents.HEALTH_BOOST);
+            list.Add(ETalents.ARMOR_BOOST);
+            list.Add(ETalents.HEALTH_BOOST_2);
+            list.Add((ETalents)ETalentsPlus.HEALTH_BOOST_3);
+            list.Add(ETalents.ARMOR_BOOST_2);
+            list.Add(ETalents.OXYGEN_TRAINING);
+            switch (RaceID)
+            {
+                case 0:     // Human Male
+                    //list.Add(ETalents.PIL_SHIP_TURNING);
+                    break;
+                case 3:     // Human Female
+                    //list.Add(ETalents.PIL_SHIP_TURNING);
+                    break;
+                case 1:     // Sylvassi
+                    //list.Add(ETalents.PIL_SHIP_TURNING);
+                    break;
+                case 2:     // Robot
+                    //list.Add(ETalents.PIL_SHIP_TURNING);
+                    break;
+            }
+            cachedTalentsForSpecies[RaceID] = list;
+            return list;
+        }
+    }
+}
---
+++ Talents/Patches.cs
@@ -0,0 +1,252 @@
+﻿using CodeStage.AntiCheat.ObscuredTypes;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static PulsarModLoader.Patches.HarmonyHelpers;
+
+namespace Talents
+{
+    [HarmonyPatch(typeof(PLPlayer), "Start")]
+    class StartTalentSizePatch
+    {
+        public static void Postfix(PLPlayer __instance)
+        {
+            __instance.Talents = new ObscuredInt[ETalentsPlus.MAX + 1];
+            __instance.TalentsLocalEditTime = new float[ETalentsPlus.MAX + 1];
+        }
+    }
+    [HarmonyPatch(typeof(PLPlayer), "SendLateInfoTo")]
+    class SendLateInfoTo
+    {
+        public static bool Prefix(PLPlayer __instance, PhotonPlayer inPlayer)
+        {
+            if (__instance.MyInventory != null)
+            {
+                __instance.MyInventory.OnLateJoin(inPlayer);
+            }
+            __instance.StartCoroutine(SafeSendLateAIInfoTo(__instance, inPlayer));
+            return false;
+        }
+        private static IEnumerator SafeSendLateAIInfoTo(PLPlayer __instance, PhotonPlayer inPlayer)
+        {
+            int loops = 0;
+            while (__instance.GetAIData() == null && loops < 50)
+            {
+                yield return new WaitForSeconds(0.5f);
+                int num = loops;
+                loops = num + 1;
+            }
+            PLPlayer playerForPhotonPlayer = PLServer.GetPlayerForPhotonPlayer(inPlayer);
+            if (playerForPhotonPlayer != null && playerForPhotonPlayer.GetClassID() == 0)
+            {
+                __instance.StartCoroutine(SendTalentsToPhotonTargets(__instance, __instance.GetClassID(), PhotonTargets.MasterClient, true, inPlayer));
+            }
+            else
+            {
+                __instance.StartCoroutine(SendPrioritiesToPhotonTargets(__instance, __instance.GetClassID(), PhotonTargets.MasterClient, true, inPlayer));
+                __instance.StartCoroutine(SendTalentsToPhotonTargets(__instance, __instance.GetClassID(), PhotonTargets.MasterClient, true, inPlayer));
+            }
+            yield break;
+        }
+        protected static FieldInfo TalentsSendTimer_MinTimeInfo = AccessTools.Field(typeof(PLPlayer), "TalentsSendTimer_MinTime");
+        protected static FieldInfo EndSyncTalentsTimeInfo = AccessTools.Field(typeof(PLPlayer), "EndSyncTalentsTime");
+        protected static FieldInfo CurrentlySendingTalentsToOthersInfo = AccessTools.Field(typeof(PLPlayer), "CurrentlySendingTalentsToOthers");
+        private static IEnumerator SendTalentsToPhotonTargets(PLPlayer __instance, int inClassID, PhotonTargets inTargets, bool sendAll = false, PhotonPlayer inPlayer = null)
+        {
+            int talentID = 0;
+            PulsarModLoader.Utilities.Logger.Info($"[TALENTS] - SendTalentsToPhotonTargets - inPlayer = null {inPlayer == null}");
+            List<ETalents> TalentList = Mod.TalentsForClass(inClassID);
+            if (inPlayer != null)
+            {
+                PulsarModLoader.Utilities.Logger.Info($"[TALENTS] - SendTalentsToPhotonTargets - PLPlayer = null {PLServer.GetPlayerForPhotonPlayer(inPlayer) == null}");
+                PulsarModLoader.Utilities.Logger.Info($"[TALENTS] - SendTalentsToPhotonTargets - ClassID = {inClassID}");
+                TalentList.AddRange(Mod.TalentsForSpecies(PLServer.GetPlayerForPhotonPlayer(inPlayer)));
+            }
+            while (talentID < ETalentsPlus.MAX && talentID < __instance.Talents.Length)
+            {
+                if (talentID == (int)ETalents.MAX)
+                {
+                    talentID++;
+                    continue;
+                }
+                if (TalentList.Contains((ETalents)talentID) && (__instance.TalentsLocalEditTime[talentID] + (float)TalentsSendTimer_MinTimeInfo.GetValue(__instance) >= (float)EndSyncTalentsTimeInfo.GetValue(__instance) || UnityEngine.Random.Range(0, 150) == 5 || sendAll))
+                {
+                    if (inPlayer != null)
+                    {
+                        __instance.photonView.RPC("GetUpdatedTalent", inPlayer, new object[]
+                        {
+                        talentID,
+                        __instance.Talents[talentID].GetDecrypted()
+                        });
+                    }
+                    else
+                    {
+                        __instance.photonView.RPC("GetUpdatedTalent", inTargets, new object[]
+                        {
+                        talentID,
+                        __instance.Talents[talentID].GetDecrypted()
+                        });
+                    }
+                    float num = 0.5f;
+                    if (__instance.IsBot)
+                    {
+                        num = 1f;
+                    }
+                    yield return new WaitForSeconds(num + PLNetworkManager.Instance.Instability);
+                }
+                talentID++;
+            }
+            CurrentlySendingTalentsToOthersInfo.SetValue(__instance, false);
+            EndSyncTalentsTimeInfo.SetValue(__instance, Time.time);
+            yield break;
+        }
+        protected static FieldInfo CurrentlySendingPrioritiesToOthersInfo = AccessTools.Field(typeof(PLPlayer), "CurrentlySendingPrioritiesToOthers");
+        private static IEnumerator SendPrioritiesToPhotonTargets(PLPlayer __instance, int inClassID, PhotonTargets inTargets, bool sendAll = false, PhotonPlayer inPlayer = null)
+        {
+            if (__instance.GetAIData() != null)
+            {
+                List<AIPriority> list = new List<AIPriority>(__instance.GetAIData().Priorities);
+                foreach (AIPriority priority in list)
+                {
+                    if (priority.NetDirty || sendAll || UnityEngine.Random.Range(0, 150) == 5)
+                    {
+                        priority.NetDirty = false;
+                        if (inPlayer != null)
+                        {
+                            __instance.photonView.RPC("GetUpdatedPriority", inPlayer, new object[]
+                            {
+                            priority.PriID,
+                            priority.BasePriority,
+                            (int)priority.Type,
+                            priority.TypeData
+                            });
+                        }
+                        else
+                        {
+                            __instance.photonView.RPC("GetUpdatedPriority", inTargets, new object[]
+                            {
+                            priority.PriID,
+                            priority.BasePriority,
+                            (int)priority.Type,
+                            priority.TypeData
+                            });
+                        }
+                        yield return new WaitForSeconds(0.05f + PLNetworkManager.Instance.Instability * 0.3f);
+                    }
+                    List<AIPriority> list2 = new List<AIPriority>(priority.Subpriorities);
+                    foreach (AIPriority aipriority in list2)
+                    {
+                        if (aipriority.NetDirty || sendAll || UnityEngine.Random.Range(0, 150) == 5)
+                        {
+                            aipriority.NetDirty = false;
+                            if (inPlayer != null)
+                            {
+                                __instance.photonView.RPC("GetUpdatedSubPriority", inPlayer, new object[]
+                                {
+                                priority.PriID,
+                                aipriority.PriID,
+                                aipriority.BasePriority,
+                                (int)aipriority.Type,
+                                aipriority.TypeData
+                                });
+                            }
+                            else
+                            {
+                                __instance.photonView.RPC("GetUpdatedSubPriority", inTargets, new object[]
+                                {
+                                priority.PriID,
+                                aipriority.PriID,
+                                aipriority.BasePriority,
+                                (int)aipriority.Type,
+                                aipriority.TypeData
+                                });
+                            }
+                            yield return new WaitForSeconds(0.05f + PLNetworkManager.Instance.Instability * 0.3f);
+                        }
+                    }
+                }
+            }
+            CurrentlySendingPrioritiesToOthersInfo.SetValue(__instance, false);
+            EndSyncTalentsTimeInfo.SetValue(__instance, Time.time);
+            yield break;
+        }
+    }
+
+    [HarmonyPatch(typeof(PLTabMenu), "UpdateTDs")]
+    class UpdateTDs
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> target = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldloc_S),       // playerFromPlayerID
+                new CodeInstruction(OpCodes.Callvirt),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PLGlobal), "TalentsForClass", new Type[] { typeof(Int32) })),
+            };
+            int NextInstruction = FindSequence(instructions, target, CheckMode.NONNULL);
+            List<CodeInstruction> patch = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),       // PLTabMenu Instance
+                instructions.ToList()[NextInstruction - 3], // playerFromPlayerID
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UpdateTDs), "Replacement", new Type[] { typeof(PLTabMenu), typeof(PLPlayer) }))
+            };
+            return PatchBySequence(instructions, target, patch, PatchMode.REPLACE, CheckMode.NONNULL);
+        }
+        public static List<ETalents> Replacement(PLTabMenu instance, PLPlayer pLPlayer)
+        {
+            List<ETalents> Talents = Mod.TalentsForClass(pLPlayer.GetClassID());
+            Talents.AddRange(Mod.TalentsForSpecies(pLPlayer));
+            return Talents;
+        }
+    }
+
+    [HarmonyPatch(typeof(PLGlobal), "GetTalentInfoForTalentType")]
+    class GetTalentInfoForTalentType
+    {
+        protected static FieldInfo CachedTalentInfosInfo = AccessTools.Field(typeof(PLGlobal), "CachedTalentInfos");
+        public static void Prefix(PLGlobal __instance, ETalents inTalent)
+        {
+            Dictionary<int, TalentInfo> CachedTalentInfos = (Dictionary<int, TalentInfo>)CachedTalentInfosInfo.GetValue(__instance);
+            if (CachedTalentInfos.ContainsKey((int)inTalent))
+            {
+                return;
+            }
+            int VanillaTalentMaxType = Enum.GetValues(typeof(ETalents)).Length;
+            int eTalentsPlus = (int)inTalent - VanillaTalentMaxType;
+            if (eTalentsPlus < 0) return;
+            TalentInfo talentInfo = new TalentInfo();
+            talentInfo.ClassID = -1;
+            talentInfo.TalentID = (int)inTalent;
+            talentInfo.MaxRank = 3;
+            talentInfo.ResearchCost = new int[6];
+            talentInfo.WarpsToResearch = 3;
+            talentInfo.ExtendsTalent = ETalents.MAX;
+            talentInfo.MinLevel = 0;
+            switch ((int)inTalent)
+            {
+                default:
+                    return;
+                case ETalentsPlus.HEALTH_BOOST_3:
+                    talentInfo.Name = "Health Boost III";
+                    talentInfo.Desc = "+20 to max health per rank";
+                    talentInfo.MaxRank = 5;
+                    talentInfo.ExtendsTalent = ETalents.HEALTH_BOOST_2;
+                    talentInfo.MinLevel = 11;
+                    break;
+            }
+            CachedTalentInfos.Add((int)inTalent, talentInfo);
+        }
+    }
+    public class ETalentsPlus
+    {
+        public const int HEALTH_BOOST_3 = (int)ETalents.MAX + 1;
+        public const int MAX = (int)ETalents.MAX + 2;
+    }
+}
